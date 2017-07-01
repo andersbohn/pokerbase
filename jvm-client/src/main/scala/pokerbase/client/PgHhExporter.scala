162f30cc2c879db8fff0363032a2447db1b7355f
Anders Bohn Jespersen <andersbohn@gmail.com>
2017-07-01 20:51:18 +0200

    upgr scala 2.11 - add quill for pg

--- jvm-client/src/main/scala/pokerbase/client/PgHhExporter.scala
+++ jvm-client/src/main/scala/pokerbase/client/PgHhExporter.scala
@@ -2,11 +2,30 @@ package pokerbase.client
 
 import java.sql.DriverManager
 
+import io.getquill._
 import pokerbase.model.HandHistory
 
 object PgHhExporter {
 
+  object Schema {
+    lazy val ctx = new PostgresJdbcContext[SnakeCase]("ctx")
+  }
+
+  case class TourneyHandHistories(idHand: String, history: String) {
+    def toHandHistory = HandHistory(idHand, history)
+
+  }
+
   def streamHandsFromPg(startAtPkId: Option[Long] = None, maxRows: Option[Long] = None): List[HandHistory] = {
+
+    import Schema.ctx._
+    val areas = quote {
+      query[TourneyHandHistories]
+    }
+    run(areas).map(_.toHandHistory)
+  }
+
+  def jdbcStreamHandsFromPg(startAtPkId: Option[Long] = None, maxRows: Option[Long] = None): List[HandHistory] = {
     Class.forName("org.postgresql.Driver")
     val url = "jdbc:postgresql://localhost:5432/PT4 DB"
 
--- project/Build.scala
+++ project/Build.scala
@@ -9,15 +9,19 @@ object ApplicationBuild extends Build {
   val ScalaVersion = "2.11.11"
 
   val appDependencies = Seq(
+    "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.5",
     "io.spray" %% "spray-json" % "1.3.3",
     //     "org.scalactic" %% "scalactic" % "3.0.1",
     // https://mvnrepository.com/artifact/postgresql/postgresql
-    "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
+//    "postgresql" % "postgresql" % "9.1-901-1.jdbc4",
+    "org.postgresql" % "postgresql" % "9.4.1208",
+    "io.getquill" %% "quill-jdbc" % "1.2.1",
+
     "org.scalatest" %% "scalatest" % "3.0.1" % "test"
   )
 
   lazy val root = Project(id = "pokerbase", base = file("."), settings = Seq(
-    scalaVersion := ScalaVersion
+    scalaVersion in ThisBuild := ScalaVersion
   ))
     .aggregate(model, jvmClient, parsers)
 
